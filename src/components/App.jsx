ce76bb73b7dfec9948915f9757cbf074759cf53a
Goahitle <nasri11france@gmail.com>
2023-09-10 22:26:32 -0700

    playing with displaying top 12 movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,59 +5,64 @@ import "../styles/App.css";
 
 function App() {
   
-  const [movies, setMovies] = useState(false);
+  // Holds api request data
+  const [movies, setMovies] = useState([]);
+
+  // Holds user inputted search data
   const [searchQuery, setSearchQuery] = useState('spiderman');
-  const endPointMovies = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}`;
+
+  // Holds a random int
   const [randomMovieIndex, setRandomMovieIndex] = useState();
+
+  // Holds random movie data
   const [randomMovie, setRandomMovie] = useState({
     title: '',
     poster: '',
     overview: ''
   });
 
+  // Api fetch link for searching
+  const endPointSearch = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}`;
 
-  useEffect( ()=>{
+  // Api fetch link for popularity
+  const endPointMovies = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${apiKey}`
+
+  useEffect(()=>{
     const fetchMovies = async () => {
       const res = await fetch(endPointMovies);
-      let data = await res.json();
-      setMovies(data);
+      let list = await res.json();
+      let shortList = list.results.slice(0, 12);
+      setMovies(shortList);
     };
 
     fetchMovies();
   },[])
 
-  function getRandomIndex(){
-    const randomIndex = Math.floor(Math.random() * movies.results.length);
-    if(randomMovieIndex === randomIndex){
-      getRandomIndex();
-    }else{
-      setRandomMovieIndex(randomIndex);
-    }
-  }
-
-  function displayRandomMovie(){
-    getRandomIndex();
-    setRandomMovie({
-      ...randomMovie,
-      title: movies.results[randomMovieIndex].title,
-      poster: imgPath + movies.results[randomMovieIndex].poster_path,
-      overview: movies.results[randomMovieIndex].overview
-    })
-  }
-
   return (
     <>
       <div className="main-wrapper">
         <h1>Movies</h1>
-        <button onClick={()=> displayRandomMovie()}>Display Random Movie</button>
-        <div>
-          <h2>{randomMovie.title}</h2>
-          <img src={randomMovie.poster} alt={randomMovie.title} 
-          style={{
-            width: 300,
-            
-          }}/>
-          <div>{randomMovie.overview}</div>
+        <div className="movie-list">
+        {movies.map((movie) => {
+          return(
+            <div
+              key={movie.id}
+              className="single-movie"
+              style={{
+                width: 300,
+              }}>
+                <h2 className="movie-title">{movie.title}</h2>
+                <img 
+                    src={`${imgPath}${movie.poster_path}`} 
+                    alt={movie.title} 
+                    className="movie-poster"
+                    />
+                <div 
+                    className="movie-overview"
+                    >{movie.overview}</div>
+            </div>
+          );
+          })} 
         </div>
       </div>
     </>
