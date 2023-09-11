a3c0bdc38131539c0a55cd04b8cdc5634f1d72e5
Goahitle <nasri11france@gmail.com>
2023-09-10 20:33:45 -0700

    playing around with displaying a random movie

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,55 @@
 import { useEffect, useState } from "react";
+import { apiKey, imgPath } from "../globals/globalVariables"
 
 import "../styles/App.css";
 
 function App() {
+  
+  const [movies, setMovies] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('spiderman');
+  const endPointMovies = `https://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${apiKey}`;
+  const [randomMovieIndex, setRandomMovieIndex] = useState();
+  const [randomMovie, setRandomMovie] = useState({
+    title: '',
+    poster: '',
+    overview: ''
+  });
 
-  const [movie, setMovie] = useState()
 
   useEffect( ()=>{
-    setMovie("Testing Branch")
+    const fetchMovies = async () => {
+      const res = await fetch(endPointMovies);
+      let data = await res.json();
+      setMovies(data);
+    };
 
+    fetchMovies();
   },[])
+
+  function displayRandomMovie(){
+    setRandomMovieIndex(Math.floor(Math.random() * movies.results.length));
+    setRandomMovie({
+      ...randomMovie,
+      title: movies.results[randomMovieIndex].title,
+      poster: imgPath + movies.results[randomMovieIndex].poster_path,
+      overview: movies.results[randomMovieIndex].overview
+    })
+  }
 
   return (
     <>
       <div className="main-wrapper">
-        <h1>{movie}</h1>
-        <p>This is the testing branch</p>
+        <h1>Movies</h1>
+        <button onClick={()=> displayRandomMovie()}>Display Random Movie</button>
+        <div>
+          <h2>{randomMovie.title}</h2>
+          <img src={randomMovie.poster} alt={randomMovie.title} 
+          style={{
+            width: 300,
+            
+          }}/>
+          <div>{randomMovie.overview}</div>
+        </div>
       </div>
     </>
   );
--- src/globals/globalVariables.js
+++ src/globals/globalVariables.js
@@ -1,2 +1,4 @@
 // Normally api keys are kept secret, but for demo purposes I will leave this here.
-export const apiKey = "";
+export const apiKey = "9de5ef6478cb7a1b5b666488d964d267";
+
+export const imgPath = "https://image.tmdb.org/t/p/original";
