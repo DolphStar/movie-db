c7e039c1a1100e73d79ae3381e2e2ccd33eecb24
Goahitle <nasri11france@gmail.com>
2023-09-13 00:44:21 -0700

    trailers are being displayed properly but the features for disabling the youtube UI are decaprecated

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,5 +1,5 @@
 import { apiKey, youtubePath, imgPath, moviePage, youtubeApiKey } from "../globals/globalVariables";
-
+import Youtube from 'react-youtube';
 import { useState, useEffect, useRef } from "react";
 
 function Quiz() {
@@ -16,15 +16,30 @@ function Quiz() {
   // Holds the youtube trailerID for the random movie video
   const [trailerID, setTrailerID] = useState('');
 
-  // Canvas reference
-  const canvasRef = useRef(null);
+  // Holds the youtube player instance
+  const [youtubePlayer, setYoutubePlayer] = useState(null);
+  
+  // Seek timestamp
+  const [seekTimestamp, setSeekTimestamp] = useState(0);
 
   // Iframe refernce
   const iframeRef = useRef(null);
 
+  // countdown screen reference
+  const countdown = useRef(null);
+
+  // Visible
+  const [isVisible, setIsVisible] = useState(false);
+
   // Gets a new random page
   function newRandomMovie() {
     setRandomPage(Math.floor(Math.random() * moviePage) + 1);
+
+    // puts a blocker in the way while the video flickers (temporary)
+    setIsVisible(true);
+    setTimeout(()=>{
+      setIsVisible(false)
+    }, 2500)
   }
 
   // Finds the trailer from the video list
@@ -74,30 +89,6 @@ function Quiz() {
     fetchMovieVideo();
   }, [randomMovie]);
 
-  // useEffect(() => {
-  //   // Function to capture a frame from the YouTube video
-  //   const captureFrame = () => {
-  //     const iframe = iframeRef.current;
-  //     const canvas = canvasRef.current;
-  //     const ctx = canvas.getContext("2d");
-
-  //     // Ensure the iframe and canvas exist
-  //     if (!iframe || !canvas) return;
-
-  //     // Get the iframe's contentWindow (the YouTube video player)
-  //     const contentWindow = iframe.contentWindow;
-
-  //     // Wait for the video to load
-  //     contentWindow.addEventListener("load", () => {
-  //       // Capture a frame from the video and draw it on the canvas
-  //       ctx.drawImage(contentWindow.document.querySelector("video"), 0, 0, canvas.width, canvas.height);
-  //     });
-  //   };
-
-  //   captureFrame();
-  // }, [randomMovie]);
-
-
   // Get the trailer when the randomMovieVideo changes
   useEffect(()=>{
     getTrailer();
@@ -106,16 +97,51 @@ function Quiz() {
   return (
     <>
       <div className="random-movie-details">
-        <h2>{randomMovie.title}</h2>  
-        <canvas className="frame" ref={canvasRef} width={650} height={350}></canvas>
-        <iframe
+
+        <h2>{randomMovie.title}</h2>
+        
+        {/* bootleg solution to the ugly video flickering that happens while the embed loads */}
+        <div 
+          className="countdown" 
+          ref={countdown}
+          style={{
+            display: isVisible ? 'block' : 'none',
+            backgroundColor: 'teal',
+          }}></div>
+
+        {/* youtube embed */}
+        <Youtube
+          className="frame"
+          videoId={trailerID}
+          opts={{
+            width: '650',
+            height: '365',
+            playerVars: {
+              autoplay: 0,  // No auto play
+              controls: 0,  // disables controls         
+              disablekb: 1, // disables keyboard controls
+              mute: 1,      // mutes
+              rel: 0
+            }
+          }}
+          onReady={(event) => {
+            setYoutubePlayer(event.target)
+            event.target.seekTo(Math.floor(Math.random() * event.target.getDuration()));
+            setTimeout(()=>{
+              event.target.pauseVideo();
+            }, 500)
+          }}
+          key={trailerID}
+        />
+        {/* <iframe
           ref={iframeRef}
           className="frame"
           src={`${youtubePath}${trailerID}`}  
           title="Embedded youtube"
-        />
+        /> */}
       </div>
       <button className="new-movie-button" onClick={()=>newRandomMovie()}>New Movie</button>
+      <button className="new-frame-button" onClick={()=>newFrame()}>New Frame</button>
     </>
   )
 }
