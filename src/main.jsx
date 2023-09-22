8a972aef62df29ccec5777e8ee2b9d198aa666a8
DolphStarX <nick.birkus@gmail.com>
2023-09-21 23:04:06 -0700

    added local storage to save favorite movies on the client side

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routers/AppRouter.jsx";
 import { FavoritesProvider } from './context/FavoritesContext.jsx'; 
-// import "./index.css";
 import "./scss/App.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
