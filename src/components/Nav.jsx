bbe62674b9e497039aaef540682af3163ae071fc
Goahitle <nasri11france@gmail.com>
2023-10-14 18:51:05 -0700

    header styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,10 +8,11 @@ function Nav() {
   return (
     <nav className='main-nav'>
         <ul>
-            <li><NavLink to='/about'>About</NavLink></li>
-            <li><NavLink to='/favorites'>Favorites</NavLink></li>
             <li className='trailr-nav'><NavLink to='/trailr'>Trailr</NavLink></li>
-            {/* <li><NavLink to='/search'>Search</NavLink></li>  */}
+            <ul className='sub-menu'>
+              <li><NavLink to='/about'>About</NavLink></li>
+              <li><NavLink to='/favorites'>Favorites</NavLink></li>
+            </ul>
         </ul>
     </nav>
   )
